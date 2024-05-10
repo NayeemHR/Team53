b12764a32ef48da8864454df4dc943e2f55b9af3
Nayeem HR <nayeemhasan042@yahoo.com>
2024-05-10 09:47:16 +0600

    11

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Custom JavaScript modifications
+    // For example, you can change the interval between slides
+    $('.carousel').carousel({
+      interval: 2000 // Change interval to 2 seconds
+    });
+  });
